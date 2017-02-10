782fd2d857c0c9da4282b0cb0a71cec705bd78ea
Thomas Moerman <thomas.moerman@gmail.com>
2017-02-10 16:49:50 +0100

    implemented takeOrAll

--- src/main/scala/org/tmoerman/brassica/util/RDDFunctions.scala
+++ src/main/scala/org/tmoerman/brassica/util/RDDFunctions.scala
@@ -6,12 +6,12 @@ import scala.reflect.ClassTag
 
 object RDDFunctions {
 
-
+  implicit def pimp[T: ClassTag](rdd: RDD[T]): RDDFunctions[T] = new RDDFunctions(rdd)
 
 }
 
 class RDDFunctions[T: ClassTag](val rdd: RDD[T]) {
 
-
+  def takeOrAll(n: Option[Int]): Array[T] = n.map(v => rdd.take(v)).getOrElse(rdd.collect)
 
 }
