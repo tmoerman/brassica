aafdc5c1160aba8bf5f4644329286816c41bf5c3
Thomas Moerman <thomas.moerman@gmail.com>
2017-02-16 17:06:27 +0100

    stitching together pipeline

--- src/main/scala/org/tmoerman/brassica/ScenicPipeline.scala
+++ src/main/scala/org/tmoerman/brassica/ScenicPipeline.scala
@@ -1,38 +1,63 @@
 package org.tmoerman.brassica
 
+import ml.dmlc.xgboost4j.scala.spark.TrackerConf
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.tmoerman.brassica.cases.DataReader
+
+import scala.concurrent.duration.{Duration, MILLISECONDS}
 
 /**
   * @author Thomas Moerman
   */
 object ScenicPipeline {
 
+  val DEFAULT_PARAMS: XGBoostParams = Map(
+    "tracker_conf" -> TrackerConf(Duration.apply(0L, MILLISECONDS), "scala")
+  )
+
   /**
+    * See XGBoost docs:
+    *   - https://github.com/dmlc/xgboost/blob/master/doc/parameter.md
+    *   - https://github.com/dmlc/xgboost/issues/332
+    *
     * @param spark
     * @param file
     * @param dataReader
     * @param transcriptionFactors
-    * @param xgBoostParams
-    * @param out
+    * @param params
+    * @param nrRounds
     */
   def apply(spark: SparkSession,
             file: String,
             dataReader: DataReader,
             transcriptionFactors: List[Gene] = Nil,
-            xgBoostParams: XGBoostParams,
-            out: String): Unit = {
+            params: XGBoostParams = DEFAULT_PARAMS,
+            nrWorkers: Int,
+            nrRounds: Int): DataFrame = {
 
     val (df, genes) = dataReader(spark, file)
 
-    // intersection of the genes in the file and the specified TFs
-    val transcriptionFactorIntersection: List[(String, Int)] =
-      genes
+    val candidateRegulators = regulatorIndices(genes, transcriptionFactors.toSet)
+
+    val grn =
+       genes
         .zipWithIndex
-        .filter{ case (gene, _) => transcriptionFactors.toSet.contains(gene) }
+        .map { case (_, targetIndex) => XGBoostRun(df, targetIndex, candidateRegulators, params, nrRounds, nrWorkers) }
+        .reduce(_ union _)
 
-
-
+    grn
   }
+
+  /**
+    * @param genes
+    * @param transcriptionFactors
+    * @return Returns the indices of the subset of genes in the DataFrame,
+    *         that also occur in the specified Set of transcription factors.
+    */
+  def regulatorIndices(genes: List[Gene], transcriptionFactors: Set[Gene]): List[Int] =
+    genes
+      .zipWithIndex
+      .filter { case (gene, _) => transcriptionFactors.toSet.contains(gene) }
+      .map(_._2)
 
 }
