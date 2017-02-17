927afd208bf569413d0b14a8285971e2ab728c92
Thomas Moerman <thomas.moerman@gmail.com>
2017-02-17 14:24:47 +0100

    accomodate for varargs

--- src/main/scala/org/tmoerman/brassica/cases/DataReader.scala
+++ src/main/scala/org/tmoerman/brassica/cases/DataReader.scala
@@ -1,6 +1,10 @@
 package org.tmoerman.brassica.cases
 
+import java.io.File
+
+import org.apache.spark.ml.attribute.AttributeGroup
 import org.apache.spark.sql._
+import org.tmoerman.brassica._
 
 /**
   * @author Thomas Moerman
@@ -11,11 +15,24 @@ trait DataReader {
     * Abstract Reader interface to pass to a pipeline.
     *
     * @param spark The SparkSession.
-    * @param file The data file.
+    * @param files The data files.
     * @return Returns a tuple:
     *         - DataFrame of the cell data. By convention, the first
     *         - List of genes.
     */
-  def apply(spark: SparkSession, file: String): (DataFrame, List[String])
+  def apply(spark: SparkSession, files: String*): (DataFrame, List[String])
+
+  /**
+    * The StructField for data Vectors.
+    */
+  val FEATURES_STRUCT_FIELD = new AttributeGroup(EXPRESSION_VECTOR).toStructField()
+
+  /**
+    * Convenience implicit conversion String -> File.
+    *
+    * @param path
+    * @return Returns java.io.File(path)
+    */
+  implicit def pimp(path: String): File = new File(path)
 
 }
