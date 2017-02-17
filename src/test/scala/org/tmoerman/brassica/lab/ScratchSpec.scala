5b428d027c401cbd6ae2136d716e272f26b035a0
Thomas Moerman <thomas.moerman@gmail.com>
2017-02-17 16:54:15 +0100

    minor

--- src/test/scala/org/tmoerman/brassica/lab/ScratchSpec.scala
+++ src/test/scala/org/tmoerman/brassica/lab/ScratchSpec.scala
@@ -1,5 +1,7 @@
 package org.tmoerman.brassica.lab
 
+import java.util.Calendar
+
 import org.scalatest.{Matchers, FlatSpec}
 
 /**
@@ -10,6 +12,14 @@ class ScratchSpec extends FlatSpec with Matchers {
   "a set" should "behave like a fn" in {
 
     Set("a", "b").apply("a")
+
+  }
+
+  "meh" should "meh" in {
+
+    val bla = Calendar.getInstance.getTime.formatted("yyyy.MM.dd.hh")
+
+    println(bla)
 
   }
 
