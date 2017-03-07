12dfaabe72e2b870050d19c312f203e0e2284b00
Thomas Moerman <thomas.moerman@gmail.com>
2017-03-07 14:28:09 +0100

    fixed test

--- src/test/scala/org/tmoerman/brassica/cases/megacell/TFMatrixReaderSpec.scala
+++ src/test/scala/org/tmoerman/brassica/cases/megacell/TFMatrixReaderSpec.scala
@@ -9,8 +9,10 @@ import org.tmoerman.brassica.ScenicPipeline.regulatorIndices
 class TFMatrixReaderSpec extends FlatSpec with Matchers {
 
   "reading the TF matrix" should "work" in {
-    val limit = Some(10) // TODO test on full!
-    
+    val limit = None // Some(10) // TODO test on full!
+
+    val (nrCells, _) = MegacellReader.readDimensions(megacell).get
+
     val genes = MegacellReader.readGeneNames(megacell).get
 
     val TFs = MegacellReader.readTFs(mouseTFs)
@@ -24,12 +26,12 @@ class TFMatrixReaderSpec extends FlatSpec with Matchers {
         .readCSCMatrix(megacell, cellTop = limit, onlyGeneIndices = pred)
         .get
 
-    csc.rows shouldBe limit.get
+    csc.rows shouldBe limit.getOrElse(nrCells)
     csc.cols shouldBe candidateRegulatorIndices.size
 
     val dm = MegacellReader.toXGBoostDMatrix(csc)
 
-    dm.rowNum shouldBe limit.get
+    dm.rowNum shouldBe limit.getOrElse(nrCells)
   }
 
 }
