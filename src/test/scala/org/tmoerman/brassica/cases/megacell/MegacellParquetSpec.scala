9db777b2b2110cba1ff0b15da1d81019e1171a01
Thomas Moerman <thomas.moerman@gmail.com>
2017-03-08 21:16:05 +0100

    aesthetics

--- src/test/scala/org/tmoerman/brassica/cases/megacell/MegacellParquetSpec.scala
+++ src/test/scala/org/tmoerman/brassica/cases/megacell/MegacellParquetSpec.scala
@@ -1,11 +1,9 @@
 package org.tmoerman.brassica.cases.megacell
 
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
-import org.apache.spark.sql.{DataFrame, SaveMode}
 import org.apache.spark.sql.SaveMode.Append
 import org.scalatest.{FlatSpec, Matchers}
-import org.tmoerman.brassica.util.TimeUtils
-import org.tmoerman.brassica.util.TimeUtils.profile
+import org.tmoerman.brassica.cases.megacell.MegacellReader._
 
 /**
   * Save to Parquet, just to try out the Spark pipeline.
@@ -19,32 +17,33 @@ class MegacellParquetSpec extends FlatSpec with DataFrameSuiteBase with Matchers
   it should "write the first 100 columns to Parquet" ignore {
     val top = 100
 
-    val csc = MegacellReader.readCSCMatrix(megacell, onlyGeneIndices = Some(0 until top)).get
+    val csc = readCSCMatrix(megacell, onlyGeneIndices = Some(0 until top)).get
 
-    val genes = MegacellReader.readGeneNames(megacell).get.take(top)
+    val genes = readGeneNames(megacell).get.take(top)
 
-    val df = MegacellReader.toColumnDataFrame(spark, csc, genes)
+    val df = toColumnDataFrame(spark, csc, genes)
 
     df.write.parquet(megacellParquet)
   }
 
   it should "write the entire matrix to Parquet" in {
-    val (nrCells, nrGenes) = MegacellReader.readDimensions(megacell).get
+    val (nrCells, nrGenes) = readDimensions(megacell).get
 
-    val genes = MegacellReader.readGeneNames(megacell).get
+    val genes = readGeneNames(megacell).get
 
     val windowSize = 1000
 
     (0 until nrGenes)
       .sliding(windowSize, windowSize)
+      .take(3)
       .foreach { range =>
         println(s"reading csc matrix columns ${range.head} -> ${range.last}")
 
         val genesInRange = genes.slice(range.head, range.head + range.size)
 
-        val csc = MegacellReader.readCSCMatrix(megacell, onlyGeneIndices = Some(range), reindex = true).get
+        val csc = readCSCMatrix(megacell, onlyGeneIndices = Some(range), reindex = true).get
 
-        val df = MegacellReader.toColumnDataFrame(spark, csc, genesInRange)
+        val df = toColumnDataFrame(spark, csc, genesInRange)
 
         println
         df.show(5)
