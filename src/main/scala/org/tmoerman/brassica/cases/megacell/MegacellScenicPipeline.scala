26d8409e584bcff5aeb9486c59ec84125ea8d858
Thomas Moerman <thomas.moerman@gmail.com>
2017-03-09 11:51:44 +0100

    evolution of megacell GRN inference

--- src/main/scala/org/tmoerman/brassica/cases/megacell/MegacellScenicPipeline.scala
+++ src/main/scala/org/tmoerman/brassica/cases/megacell/MegacellScenicPipeline.scala
@@ -1,11 +1,15 @@
 package org.tmoerman.brassica.cases.megacell
 
-import breeze.linalg.{Matrix, SparseVector, _}
-import org.apache.spark.sql.SparkSession
+import _root_.ml.dmlc.xgboost4j.scala.{DMatrix, XGBoost}
+import breeze.linalg.{CSCMatrix, DenseMatrix}
+import org.apache.spark.ml.linalg.SparseVector
+import org.apache.spark.sql.functions.desc
+import org.apache.spark.sql.{Row, SparkSession}
 import org.tmoerman.brassica.ScenicPipeline._
+import org.tmoerman.brassica._
 import org.tmoerman.brassica.cases.megacell.MegacellReader._
-import ml.dmlc.xgboost4j.scala.{DMatrix, XGBoost}
-import org.tmoerman.brassica.{ExpressionValue, Gene, XGBoostParams}
+
+import scala.collection.immutable.ListMap
 
 /**
   * @author Thomas Moerman
@@ -17,7 +21,7 @@ object MegacellScenicPipeline {
             parquetPath: String,
             candidateRegulators: List[Gene],
             targets: List[Gene] = Nil,
-            params: XGBoostParams = XGBoostParams(),
+            params: RegressionParams = RegressionParams(),
             normalize: Boolean = true): Unit = {
 
     val sc = spark.sparkContext
@@ -32,59 +36,70 @@ object MegacellScenicPipeline {
 
     val isTarget = toPredicate(targets)
 
-    val grn =
+    val GRN =
       spark
         .read
         .parquet(parquetPath)
         .rdd
-        .filter(row => {
-          val rowGene: Gene = ???
-          isTarget(rowGene) })
-        .flatMap(row => {
-          val csc = cscBroadcast.value
+        .filter(row => isTarget(row.gene))
+        .flatMap(row => importanceScores(row.gene, row.data, indexBroadcast.value, cscBroadcast.value, params))
 
-          val targetGene: Gene = ???
-          val responseVector: SparseVector[ExpressionValue] = ???
-
-          val cleanCSCIndexTuples =
-            indexBroadcast
-              .value.keys.zipWithIndex
-              .filterNot{ case (gene, _) => gene == targetGene }
-              .toSeq
-
-          // prep training data
-          val predictorCSC = csc.apply(0 until csc.rows, cleanCSCIndexTuples.map(_._2)).toDenseMatrix
-          val trainingData = toDMatrix(predictorCSC)
-          val y = responseVector.toDenseVector.data.map(_.toFloat)
-          trainingData.setLabel(y)
-
-          // train the model
-          val booster =
-            XGBoost
-              .train(
-                trainingData,
-                params.boosterParams,
-                params.nrRounds)
-
-          // extract the importance scores.
-          val sum = booster.getFeatureScore().values.map(_.toInt).sum
-
-          booster
-            .getFeatureScore()
-            .map{ case (feature, score) => {
-              val featureIndex = feature.substring(1).toInt
-              val (regulatorGene, _) = cleanCSCIndexTuples(featureIndex)
-              val importance = if (normalize) score.toFloat / sum else score.toFloat
-
-              (regulatorGene, targetGene, importance)
-            }}})
+    spark
+      .createDataFrame(GRN)
+      .toDF(CANDIDATE_REGULATOR_NAME, TARGET_GENE_NAME, IMPORTANCE)
+      .sort(desc(IMPORTANCE))
   }
 
-  private[this] def toPredicate(targets: List[Gene]) = targets match {
+  private[megacell] implicit class PimpRow(row: Row) {
+    def gene: String = row.getAs[String](GENE)
+    def data: Array[Float] = row.getAs[SparseVector](VALUES).toArray.map(_.toFloat)
+  }
+
+  private[megacell] def importanceScores(targetGene: String,
+                                     responseVector: Array[Float],
+                                     index: ListMap[Gene, GeneIndex],
+                                     csc: CSCMatrix[GeneExpression],
+                                     params: RegressionParams) = {
+    import params._
+
+    // prepare training data
+    val cleanCSCIndexTuples =
+      index
+        .keys
+        .zipWithIndex
+        .filterNot { case (gene, _) => gene == targetGene }
+        .toSeq
+
+    // compile training data
+    val predictorCSC = csc.apply(0 until csc.rows, cleanCSCIndexTuples.map(_._2)).toDenseMatrix
+    val trainingData = toDMatrix(predictorCSC)
+    trainingData.setLabel(responseVector)
+
+    // train the model
+    val booster = XGBoost.train(trainingData, boosterParams, nrRounds)
+
+    // extract the importance scores.
+    val sum = booster.getFeatureScore().values.map(_.toInt).sum
+    val importanceScores =
+      booster
+        .getFeatureScore()
+        .map { case (feature, score) => {
+          val featureIndex = feature.substring(1).toInt
+          val (regulatorGene, _) = cleanCSCIndexTuples(featureIndex)
+          val importance = if (normalize) score.toFloat / sum else score.toFloat
+
+          (regulatorGene, targetGene, importance)
+        }}
+
+    importanceScores
+  }
+
+  private[megacell] def toPredicate(targets: List[Gene]) = targets match {
     case Nil => (_: Gene) => true
     case _ => targets.toSet.contains _
   }
 
-  def toDMatrix(dm: DenseMatrix[Int]) = new DMatrix(dm.data.map(_.toFloat), dm.rows, dm.cols, 0f)
+  private[megacell] def toDMatrix(dm: DenseMatrix[GeneExpression]) =
+    new DMatrix(dm.data.map(_.toFloat), dm.rows, dm.cols, 0f)
 
 }
