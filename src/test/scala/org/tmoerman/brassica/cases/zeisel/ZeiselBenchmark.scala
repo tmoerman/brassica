9f0f1befa8cb34b2feeaee56e95d741602a8485c
Thomas Moerman <thomas.moerman@gmail.com>
2017-03-17 23:22:04 +0100

    correct targets in benchmark

--- src/test/scala/org/tmoerman/brassica/cases/zeisel/ZeiselBenchmark.scala
+++ src/test/scala/org/tmoerman/brassica/cases/zeisel/ZeiselBenchmark.scala
@@ -27,10 +27,8 @@ class ZeiselBenchmark extends FlatSpec with XGBoostSuiteBase with Matchers {
 
     val genes = ZeiselReader.readGenes(spark, zeiselMrna)
 
-    // val nrTargets = Seq(1, 5, 10, 25, 100, 250, 1000, 2500, 10000, genes.size)
-    // val nrTargets = Seq(2500, 10000, genes.size)
-    val nrTargets = Seq(2500)
-
+    val nrTargets = Seq(1, 5, 10, 25, 100, 250, 1000, 2500, 10000, genes.size)
+    
     val machine = "giorgio"
 
     val durations =
