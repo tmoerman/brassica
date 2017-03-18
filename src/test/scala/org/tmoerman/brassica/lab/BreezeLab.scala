4a7b1948c341e3fec2693e1b9b760f28fa6352d7
Thomas Moerman <thomas.moerman@gmail.com>
2017-03-18 12:39:03 +0100

    meh

---
+++ src/test/scala/org/tmoerman/brassica/lab/BreezeLab.scala
@@ -0,0 +1,14 @@
+package org.tmoerman.brassica.lab
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+  * Created by tmo on 18/03/17.
+  */
+class BreezeLab extends FlatSpec with Matchers {
+
+  "adding two sparse matrices" should "work" in {
+
+  }
+
+}
