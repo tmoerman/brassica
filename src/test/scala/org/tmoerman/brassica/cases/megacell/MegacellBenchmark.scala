0fcbf072366464535a3bef6029c1df3a6553a91e
Thomas Moerman <thomas.moerman@gmail.com>
2017-03-19 21:51:30 +0100

    benchmark test case for Megacell

---
+++ src/test/scala/org/tmoerman/brassica/cases/megacell/MegacellBenchmark.scala
@@ -0,0 +1,46 @@
+package org.tmoerman.brassica.cases.megacell
+
+import org.scalatest.FlatSpec
+import org.tmoerman.brassica.{RegressionParams, XGBoostSuiteBase}
+
+/**
+  * @author Thomas Moerman
+  */
+class MegacellBenchmark extends FlatSpec with XGBoostSuiteBase {
+
+  val path = "/media/tmo/data/work/datasets/megacell_parquet_full"
+
+  val boosterParams = Map(
+    "seed" -> 777,
+    "silent" -> 1,
+    "eta" -> 0.2,
+    "subsample" -> 0.8,
+    "colsample_bytree" -> 0.7,
+    "gamma" -> 2
+  )
+
+  val params =
+    RegressionParams(
+      normalize = true,
+      nrRounds = 25,
+      boosterParams = boosterParams)
+
+  "the Megacell emb.par pipeline from parquet" should "run" in {
+    val TFs = MegacellReader.readTFs(mouseTFs).toSet
+
+    val result =
+      MegacellPipeline
+        .apply(
+          spark,
+          hdf5 = megacell,
+          parquet = path,
+          candidateRegulators = TFs,
+          targets = Set("Gad1"),
+          params = params)
+
+    println(params)
+
+    result.show()
+  }
+
+}
