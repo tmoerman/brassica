c7618327e85166aba73c56dec872b518ffaea21f
Thomas Moerman <thomas.moerman@gmail.com>
2017-03-21 13:32:06 +0100

    getting rid of irrelevant Zeisel reader logic

--- src/main/scala/org/tmoerman/brassica/cases/zeisel/ZeiselReader.scala
+++ src/main/scala/org/tmoerman/brassica/cases/zeisel/ZeiselReader.scala
@@ -1,15 +1,11 @@
 package org.tmoerman.brassica.cases.zeisel
 
-import breeze.linalg.{CSCMatrix, SparseVector => BSV}
-import org.apache.spark.ml.linalg.BreezeMLConversions._
+import breeze.linalg.{SparseVector => BSV}
 import org.apache.spark.ml.linalg.{Vectors, Vector => MLVector}
 import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.types.{StructField, _}
-import org.apache.spark.sql.{DataFrame, Dataset, Row, SparkSession}
+import org.apache.spark.sql.{Dataset, SparkSession}
 import org.tmoerman.brassica._
 import org.tmoerman.brassica.cases.DataReader
-
-import scala.reflect.ClassTag
 
 /**
   * Read the Zeisel mRNA expression data a Spark SQL DataFrame.
@@ -35,18 +31,21 @@ object ZeiselReader extends DataReader {
 
   private[zeisel] val NR_META_FEATURES  = 10
   private[zeisel] val EMPTY_LINE_INDEX  = NR_META_FEATURES
-  private[zeisel] val FEAT_INDEX_OFFSET = NR_META_FEATURES + 1
-  private[zeisel] val OBSERVATION_INDEX_OFFSET  = 2
+//  private[zeisel] val FEAT_INDEX_OFFSET = NR_META_FEATURES + 1
+//  private[zeisel] val OBSERVATION_INDEX_OFFSET  = 2
 
   /**
     * @param spark The SparkSession.
-    * @param raw The raw Zeisel file.
-    * @return Returns a tuple:
-    *         - DataFrame of the Zeisel expression mRNA data with schema
-    *         - Gene list
+    * @param raw The raw Zeisel mRNAExpression file.
+    * @return
     */
-  def apply(spark: SparkSession, raw: Path): (DataFrame, List[Gene]) =
-    apply(spark, rawLines(spark, raw).cache)
+  def apply(spark: SparkSession, raw: Path): Dataset[ExpressionByGene] = {
+    import spark.implicits._
+
+    rawLines(spark, raw)
+      .flatMap(toExpressionByGene)
+      .toDS
+  }
 
   /**
     * @param spark The SparkSession.
@@ -63,166 +62,6 @@ object ZeiselReader extends DataReader {
         (split, idx) }
       .filter(_._2 != EMPTY_LINE_INDEX)
 
-  /**
-    * @param spark The SparkSession.
-    * @param lines The (cached) lines parsed from the raw Zeisel file.
-    * @return Returns a tuple:
-    *         - DataFrame of the Zeisel expression mRNA data with schema
-    *         - Gene list
-    */
-  def apply(spark: SparkSession, lines: RDD[Line]): (DataFrame, List[Gene]) = {
-    val genes = readGenes(lines)
-
-    val schema = parseSchema(lines)
-
-    val nrExpressionFeatures = lines.count.toInt - NR_META_FEATURES
-
-    val rows = parseRows(lines, nrExpressionFeatures)
-
-    val df =
-      spark
-        .createDataFrame(rows, schema)
-        .na.fill(0) // TODO is this necessary?
-
-    (df, genes)
-  }
-
-  /**
-    * @param lines The RDD of lines.
-    * @return Returns the List of Gene names.
-    */
-  private[zeisel] def readGenes(lines: RDD[Line]): List[Gene] =
-    lines
-      .filter(_._2 >= FEAT_INDEX_OFFSET)
-      .map(_._1.head)
-      .collect
-      .toList
-
-  /**
-    * @param spark The SparkSession.
-    * @param parquetFile The Zeisel parquet file.
-    * @param rawFile The raw Zeisel data file.
-    * @return Returns the Zeisel gene expression DataFrame from a parquet file.
-    */
-  def fromParquet(spark: SparkSession, parquetFile: String, rawFile: String): (DataFrame, List[Gene]) = {
-    val df = spark.read.parquet(parquetFile).cache
-
-    val genes = readGenes(spark, rawFile)
-
-    (df, genes)
-  }
-
-  /**
-    * @param spark The SparkSession.
-    * @param raw The raw Zeisel file path.
-    * @return Returns the List of Gene names.
-    */
-  private[zeisel] def readGenes(spark: SparkSession, raw: Path): List[Gene] = {
-
-    // left of the separator Char
-    def leftOf(c: Char)(s: String) = s.splitAt(s.indexOf(c))._1
-
-    spark
-      .sparkContext
-      .textFile(raw)
-      .zipWithIndex
-      .filter{ case (_, idx) => idx >= FEAT_INDEX_OFFSET }
-      .map{ case (s, _) => leftOf('\t')(s)}
-      .collect
-      .toList
-  }
-
-  /**
-    * @param lines The RDD of raw lines.
-    * @return Returns the schema StructType.
-    */
-  private[zeisel] def parseSchema(lines: RDD[Line]): StructType = {
-
-    def clean(name: String) = name.replace(' ', '_').replace("#", "count")
-
-    val meta =
-      lines
-        .take(NR_META_FEATURES)
-        .map {
-          case (_ :: name :: _, 0l | 7l | 8l | 9l)      => StructField(clean(name), StringType,  nullable = false)
-          case (_ :: name :: _, 1l | 2l | 3l | 4l | 5l) => StructField(clean(name), IntegerType, nullable = false)
-          case (_ :: name :: _, 6l)                     => StructField(clean(name), FloatType,   nullable = false)
-          case _ => ???
-        }
-        .toList
-
-    StructType(EXPRESSION_STRUCT_FIELD :: meta)
-  }
-
-  /**
-    * @param lines The RDD of lines.
-    * @param expressionVectorLength The length of a gene expression vector.
-    * @param na The value to consider as N/A.
-    * @return Returns row instances where the first entry in each row is the sparse expression vector,
-    *         followed by the meta attributes.
-    */
-  private[zeisel] def parseRows(lines: RDD[Line],
-                                expressionVectorLength: Count,
-                                na: Option[Int] = Some(0), // TODO necessary?
-                                nrCells: Option[Int] = None): RDD[Row] = {
-
-    type ACC = (Array[Any], BSV[Double])
-
-    def init(entry: (Any, Index)): ACC = {
-      val meta     = Array.ofDim[Any](NR_META_FEATURES)
-      val features = BSV.zeros[Double](expressionVectorLength)
-      val acc      = (meta, features)
-
-      update(acc, entry)
-    }
-
-    def update(acc: ACC, entry: (Any, Index)): ACC = acc match { case (meta, features) =>
-      (entry: @unchecked) match {
-        case (v, metaIdx) if metaIdx < NR_META_FEATURES => meta.update(metaIdx.toInt, v)
-        case (v: Double, featIdx) =>
-          features.update(featIdx.toInt - FEAT_INDEX_OFFSET, v)
-      }
-
-      acc
-    }
-
-    def merge(a: ACC, b: ACC) = (a, b) match { case ((a_meta, a_features), (b_meta, b_features)) =>
-      val meta = (a_meta zip b_meta).map{
-        case (l, null) => l
-        case (null, r) => r
-        case _         => null
-      }
-
-      val features = a_features += b_features
-
-      (meta, features)
-    }
-
-    implicit class PimpCols[T: ClassTag](list: List[T]) {
-      def prep: List[T] = {
-        val result = list.drop(OBSERVATION_INDEX_OFFSET)
-
-        nrCells.map(n => result.take(n)).getOrElse(result)
-      }
-    }
-
-    lines
-      .flatMap {
-        case (cols, metaIdx @ (0l | 7l | 8l | 9l))      => cols.prep.zipWithIndex.map     { case (v, cellIdx) => (cellIdx, (v,         metaIdx)) }
-        case (cols, metaIdx @ (1l | 2l | 3l | 4l | 5l)) => cols.prep.zipWithIndex.map     { case (v, cellIdx) => (cellIdx, (v.toInt,   metaIdx)) }
-        case (cols, metaIdx @ 6l)                       => cols.prep.zipWithIndex.map     { case (v, cellIdx) => (cellIdx, (v.toFloat, metaIdx)) }
-        case (cols, featIdx)                            => cols.prep.zipWithIndex.flatMap { case (v, cellIdx) =>
-          if (na.contains(v.toInt))
-            Seq.empty
-          else
-            Seq((cellIdx, (v.toDouble, featIdx)))
-        }}
-      .combineByKey(init, update, merge)
-      .sortByKey()
-      .values
-      .map { case (meta, features) => Row.fromSeq(features.ml :: meta.toList) }
-  }
-
   private[zeisel] def toExpressionByGene(line: Line): Option[ExpressionByGene] =
     Some(line)
       .filter(_._2 >= NR_META_FEATURES)
@@ -232,13 +71,13 @@ object ZeiselReader extends DataReader {
       }
 
   private[zeisel] def toExpressionVector(values: List[String]): MLVector = {
-    val elements =
+    val tuples =
       values
         .zipWithIndex
         .filterNot(_._1 == "0")
         .map { case (value, cellIdx) => (cellIdx, value.toDouble) }
 
-    Vectors.sparse(values.length, elements)
+    Vectors.sparse(values.length, tuples)
   }
 
 }
--- src/test/scala/org/tmoerman/brassica/cases/zeisel/ZeiselReaderSpec.scala
+++ src/test/scala/org/tmoerman/brassica/cases/zeisel/ZeiselReaderSpec.scala
@@ -1,7 +1,5 @@
 package org.tmoerman.brassica.cases.zeisel
 
-import java.io.File
-
 import com.holdenkarau.spark.testing.DataFrameSuiteBase
 import org.scalatest.{FlatSpec, Matchers}
 import org.tmoerman.brassica.cases.zeisel.ZeiselReader._
@@ -13,42 +11,6 @@ class ZeiselReaderSpec extends FlatSpec with DataFrameSuiteBase with Matchers {
 
   behavior of "ZeiselReader"
 
-  it should "parse the schema correctly" in {
-    val lines = rawLines(spark, zeiselMrna)
-
-    val schema = parseSchema(lines)
-
-    schema.size shouldBe 11
-
-    schema.exists(_.name == "cell_id") shouldBe true
-
-    schema.exists(_.name == "expression") shouldBe true
-  }
-
-  val FIRST_FIVE_GENES = List("Tspan12", "Tshz1", "Fnbp1l", "Adamts15", "Cldn12")
-
-  it should "parse the gene names correctly" in {
-    val lines = rawLines(spark, zeiselMrna)
-
-    val genes = readGenes(lines)
-
-    genes.take(5) shouldBe FIRST_FIVE_GENES
-  }
-
-  it should "parse the gene names quickly" in {
-    val genes = readGenes(spark, zeiselMrna)
-
-    genes.take(5) shouldBe FIRST_FIVE_GENES
-  }
-
-  it should "parse the DataFrame correctly" in {
-    val (df, _) = apply(spark, zeiselMrna)
-
-    df.count shouldBe ZEISEL_CELL_COUNT
-
-    df.show(5)
-  }
-
   it should "parse the mouse TFs properly" in {
     val TFs = readTFs(mouseTFs)
 
@@ -58,52 +20,13 @@ class ZeiselReaderSpec extends FlatSpec with DataFrameSuiteBase with Matchers {
   it should "read column vectors correctly" in {
     import spark.implicits._
 
-    val lines = ZeiselReader.rawLines(spark, zeiselMrna)
-
-    val ds = lines.flatMap(toExpressionByGene).toDS
+    val ds = ZeiselReader.apply(spark, zeiselMrna)
 
     ds.filter($"gene" === "Dlx1").show()
 
     ds.head.values.size shouldBe ZEISEL_CELL_COUNT
 
     ds.count shouldBe ZEISEL_GENE_COUNT
-  }
-
-  it should "read CSC matrix (cells * genes)" in {
-    // FIXME
-
-//    val lines = ZeiselReader.rawLines(spark, zeiselMrna)
-//
-//    val onlyGenes = Seq(3, 4, 5)
-//
-//    val csc = ZeiselReader.toCSCMatrix(
-//      lines,
-//      onlyGenes,
-//      nrCells = ZEISEL_CELL_COUNT)
-//
-//    csc.cols shouldBe onlyGenes.size
-//
-//    csc.rows shouldBe ZEISEL_CELL_COUNT
-  }
-
-  behavior of "Zeisel Parquet I/O"
-
-  it should "write the gene expression DF to parquet" in {
-    if (! new File(zeiselParquet).exists) {
-      val (df, _) = apply(spark, zeiselMrna)
-
-      df.write.parquet(zeiselParquet)
-    }
-  }
-
-  it should "read the gene expression DF from .parquet" in {
-    val (df, genes) = fromParquet(spark, zeiselParquet, zeiselMrna)
-
-    df.show(5)
-
-    df.count shouldBe ZEISEL_CELL_COUNT
-
-    genes.size shouldBe ZEISEL_GENE_COUNT
   }
 
 }
