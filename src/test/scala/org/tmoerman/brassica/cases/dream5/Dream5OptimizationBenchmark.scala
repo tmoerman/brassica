4446d992e6a18744ec254bdeb2a059c320d07cde
Thomas Moerman <thomas.moerman@gmail.com>
2017-04-07 21:58:46 +0200

    grouped aggregation statistics on a dataframe

--- src/test/scala/org/tmoerman/brassica/cases/dream5/Dream5OptimizationBenchmark.scala
+++ src/test/scala/org/tmoerman/brassica/cases/dream5/Dream5OptimizationBenchmark.scala
@@ -12,26 +12,23 @@ class Dream5OptimizationBenchmark extends FlatSpec with XGBoostSuiteBase with Ma
 
   val writePath = props("dream5Optimization")
 
-  "Dream5 hyperparam optimization" should "run" in {
+  "Dream5 hyperparam optimization" should "run" ignore {
     Seq(3).foreach(optimizeHyperParams)
   }
 
   val optimizationParams: XGBoostOptimizationParams =
     XGBoostOptimizationParams(
-      nrTrialsPerBatch = 2,
-      nrBatches = 8,
-      parallel = false,
+      nrTrialsPerBatch = 10000 / 88,
+      nrBatches = 88,
       onlyBestTrial = false)
 
   val nrCores = Runtime.getRuntime.availableProcessors()
-
-  println(s"available processors: $nrCores")
 
 //  val TF_50     = (  1 to   0 + (nrCores / 2)).map(i => s"G$i").toList
 //  val NORMAL_50 = (501 to 500 + (nrCores / 2)).map(i => s"G$i").toList
 //  val TARGETS   = TF_50 ::: NORMAL_50
 
-  val TARGETS = "G666" :: Nil
+  val TARGETS = "G3" :: Nil
 
   private def optimizeHyperParams(idx: Int): Unit = {
     import spark.implicits._
@@ -57,7 +54,32 @@ class Dream5OptimizationBenchmark extends FlatSpec with XGBoostSuiteBase with Ma
     optimizedHyperParamsDS
       .write
       .mode(Overwrite)
-      .parquet(writePath + s"network${idx}_low_gamma")
+      .parquet(writePath + s"network${idx}_small")
+  }
+
+  // http://stackoverflow.com/questions/31789939/calculate-the-standard-deviation-of-grouped-data-in-a-spark-dataframe
+  "adding a standardized loss column" should "work" in {
+
+    import spark.implicits._
+    import org.apache.spark.sql.functions._
+
+    val ds = spark.read.parquet(writePath + s"network3_small").cache
+
+    val statsByTarget =
+      ds
+        .groupBy($"target")
+        .agg(
+          stddev("loss").as("stddev_loss"),
+          mean("loss").as("mean_loss"))
+
+    statsByTarget.show
+
+    ds
+      .join(statsByTarget, ds("target") === statsByTarget("target"), "outer")
+      .withColumn("std_loss", ($"loss" - $"mean_loss") / $"stddev_loss")
+      .sort($"std_loss")
+      .show
+
   }
 
 }
