256ce0bb9d4be1e9bc5bb66c6f93a99866aae1aa
Thomas Moerman <thomas.moerman@gmail.com>
2017-04-15 19:34:24 +0200

    megacell spark-submit script

---
+++ launch_megacell.sh
@@ -0,0 +1,21 @@
+echo "SBT .jar assembly"
+sbt assembly
+
+echo "submitting Spark job"
+$SPARK_HOME/bin/spark-submit \
+  --class org.tmoerman.brassica.cases.megacell.MegacellInference \
+  --master local[*] \
+  --deploy-mode client \
+  --driver-memory 96g \
+  --conf spark.executor.memory=96g \
+  --conf spark.driver.maxResultSize=20gb \
+  --conf spark.network.timeout=10000000 \
+  ./target/scala-2.11/gradinets.jar \
+  /media/tmo/data/work/datasets/megacell/parquet_full \
+  /home/tmo/work/batiskav/projects/brassica/src/test/resources/TF/mm9_TFs.txt \
+  /media/tmo/data/work/datasets/megacell/out \
+  100000 \
+  1000 \
+  8 \
+  11 \
+  12
