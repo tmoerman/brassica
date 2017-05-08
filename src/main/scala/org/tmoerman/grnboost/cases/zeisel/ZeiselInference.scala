95a0a4fecd1029a9025864fb5c92bfe38a0475ed
Thomas Moerman <thomas.moerman@gmail.com>
2017-05-08 23:00:45 +0200

    measure wall time + other seed

--- src/main/scala/org/tmoerman/grnboost/cases/zeisel/ZeiselInference.scala
+++ src/main/scala/org/tmoerman/grnboost/cases/zeisel/ZeiselInference.scala
@@ -4,14 +4,16 @@ import org.apache.commons.io.FileUtils.deleteDirectory
 import org.apache.spark.sql.SparkSession
 import org.tmoerman.grnboost._
 import org.tmoerman.grnboost.cases.DataReader._
+import org.tmoerman.grnboost.util.TimeUtils
+import org.tmoerman.grnboost.util.TimeUtils.pretty
 
 /**
   * @author Thomas Moerman
   */
 object ZeiselInference {
-  
+
   val boosterParams = Map(
-    "seed" -> 777,
+    "seed" -> 666,
     "eta" -> 0.15,
     "subsample" -> 0.8,
     "colsample_bytree" -> 0.8,
@@ -27,48 +29,54 @@ object ZeiselInference {
       boosterParams = boosterParams)
 
   def main(args: Array[String]): Unit = {
-    val in           = args(0)
-    val mouseTFs     = args(1)
-    val out          = args(2)
-    val nrPartitions = args(3).toInt
-    val nrThreads    = args(4).toInt
 
-    val parsed =
-      s"""
-         |Args:
-         |* in              = $in
-         |* mouseTFs        = $mouseTFs
-         |* output          = $out
-         |* nr partitions   = $nrPartitions
-         |* nr xgb threads  = $nrThreads
+    val (_, duration) = TimeUtils.profile {
+      val in           = args(0)
+      val mouseTFs     = args(1)
+      val out          = args(2)
+      val nrPartitions = args(3).toInt
+      val nrThreads    = args(4).toInt
+
+      val parsed =
+        s"""
+           |Args:
+           |* in              = $in
+           |* mouseTFs        = $mouseTFs
+           |* output          = $out
+           |* nr partitions   = $nrPartitions
+           |* nr xgb threads  = $nrThreads
       """.stripMargin
 
-    println(parsed)
+      println(parsed)
 
-    deleteDirectory(out)
+      deleteDirectory(out)
 
-    val spark =
-      SparkSession
-        .builder
-        .appName(GRN_BOOST)
-        .getOrCreate
+      val spark =
+        SparkSession
+          .builder
+          .appName(GRN_BOOST)
+          .getOrCreate
 
-    val ds  = readExpression(spark, in).cache
-    val TFs = readTFs(mouseTFs).toSet
+      val ds  = readExpression(spark, in).cache
+      val TFs = readTFs(mouseTFs).toSet
 
-    val regulations =
-      GRNBoost
-        .inferRegulations(
-          ds,
-          candidateRegulators = TFs,
-          params = params.copy(
-            boosterParams = params.boosterParams + (XGB_THREADS -> nrThreads)),
-          nrPartitions = Some(nrPartitions))
-        .cache
+      val regulations =
+        GRNBoost
+          .inferRegulations(
+            ds,
+            candidateRegulators = TFs,
+            params = params.copy(
+              boosterParams = params.boosterParams + (XGB_THREADS -> nrThreads)),
+            nrPartitions = Some(nrPartitions))
+          .cache
 
-    regulations
-      .normalize(params)
-      .saveTxt(out)
+      regulations
+        .normalize(params)
+        .saveTxt(out)
+    }
+
+    println(s"wall time: ${pretty(duration)}")
+
   }
 
 }
