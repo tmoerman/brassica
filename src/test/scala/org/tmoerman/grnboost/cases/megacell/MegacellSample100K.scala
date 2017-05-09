45038c76c772b466b651b6e54ba57f0978bb383b
Thomas Moerman <thomas.moerman@gmail.com>
2017-05-09 15:58:04 +0200

    fix compilation error in test

--- src/test/scala/org/tmoerman/grnboost/cases/megacell/MegacellSample100K.scala
+++ src/test/scala/org/tmoerman/grnboost/cases/megacell/MegacellSample100K.scala
@@ -9,7 +9,7 @@ import org.tmoerman.grnboost.{GRNBoostSuiteBase, randomSubset}
 class MegacellSample100K extends FlatSpec with GRNBoostSuiteBase with Matchers {
 
   "it" should "work" in {
-    val subset = randomSubset(nr, 0 until totalCellCount)
+    //val subset = randomSubset(nr, 0 until totalCellCount)
 
 
   }
