6d99cf595e4c1394604237c90e42b31d24ac1314
Thomas Moerman <thomas.moerman@gmail.com>
2017-06-28 16:06:02 +0200

    preliminary xgbfi functions

---
+++ src/main/scala/org/aertslab/grnboost/xgbfi/Tree.scala
@@ -0,0 +1,17 @@
+package org.aertslab.grnboost.xgbfi
+
+/**
+  * Inspired by: https://gist.github.com/dholbrook/2967371
+  *
+  * @author Thomas Moerman
+  */
+//abstract class Tree[+T](value: Option[T],
+//                        l: Option[Tree[T]],
+//                        r: Option[Tree[T]]) {
+//
+//}
+
+// TODO
+// Q: what is the leaf value?
+// case class Node[T](v: T, l: Tree[T], r: Tree[T]) extends Tree(Some(v), l, r)
+// case class Leaf[T](v: T) extends Tree(Some())
