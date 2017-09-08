f2db95b0cbce13ba3822600436298ddb6f1cab10
Thomas Moerman <thomas.moerman@gmail.com>
2017-09-08 14:09:53 +0200

    test again on Macosko full -> cfg run

---
+++ cli_macosko_full_cfg_run.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+echo "SBT .jar assembly"
+sbt assembly
+
+echo "Launching cfg-run"
+$SPARK_HOME/bin/spark-submit \
+  --class org.aertslab.grnboost.GRNBoost \
+  --master local[*] \
+  --deploy-mode client \
+  --driver-memory 96g \
+  --conf spark.executor.memory=96g \
+  --conf spark.driver.maxResultSize=20gb \
+  --conf spark.network.timeout=10000000 \
+  --conf spark.eventLog.enabled=true \
+  ./target/scala-2.11/GRNBoost.jar \
+  infer \
+  -i  /media/tmo/data/work/datasets/macosko/in/allEsetMR.tsv \
+  -tf /home/tmo/work/batiskav/projects/brassica/src/test/resources/TF/mm9_TFs.txt \
+  -o  /media/tmo/data/work/datasets/macosko/out/8ab4224/full \
+  --regularized \
+  --truncate 100000 \
+  --cfg-run
